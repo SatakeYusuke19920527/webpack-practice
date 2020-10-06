10800ffb18d7fdd7451490c02e1d599a70d3b7ad
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-10-06 21:16:20 +0900

    App.tsx functionComponent React.FC error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CnButton from './components/CnButton';
 
-const App = () => (
+const App: React.FC<{}> = () => (
   <div>
     <h1>Hello React TypeScript</h1>
     <CnButton />
