6d2b2277e2d62e7c4b7596e1c47e52d432fd7e77
satake yusuke <satake@osa.connected.co.jp>
2020-10-06 11:06:05 +0900

    install react

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,44 @@
 const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: outputPath,
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(jpe?g|png|gif|gif|svg|ico)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 2048,
-          name: './images/[name].[ext]',
-        },
-      },
-    ],
-  },
-  devServer: {
-    contentBase: outputPath,
-  },
+    entry: './src/index.js',
+    output: {
+        path: outputPath,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|gif|svg|ico)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 2048,
+                    name: './images/[name].[ext]',
+                },
+            },
+            {
+                test: /\.(jsx?|tsx?)$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            }
+        ],
+    },
+    devServer: {
+        contentBase: outputPath,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
 };
