c4537bb250559ec1c9ba2ce5d10a1d1bfabcd312
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-10-06 09:13:23 +0900

    console.log削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist');
-console.log(outputPath, 'outputPath');
 module.exports = {
   entry: './src/index.js',
   output: {
